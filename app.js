6c4c72ce59d268117612e84b27a531dd9f85b079
Bassel Alrefai <bassel@basselalrefai.com>
2021-01-14 15:23:04 -0500

    Create dynamically generated divs

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 //UI variables
+const divContainer = document.getElementById("container");
 
+//Create Divs
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.classList.add("sketch-square");
+  divContainer.appendChild(div);
+}
 //Event handlers
